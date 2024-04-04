8730ca86d950a0fd07b967f17c1c50f8f112c0ce
ArtemLobastov <artemlobastov92@gmail.com>
2024-04-04 02:09:18 +0200

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobaStyles';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-`;
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
 
 const StyledApp = styled.div`
   background-color: red;
@@ -32,10 +14,13 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello world</H1>
-        <Button>Push me</Button>
-
-        <Input type="number" placeholder="Enter your number" />
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Here is h2 world</Heading>
+        <Button>Check in</Button>
+        <Button>Check out</Button>
+        <Heading as="h3">Here is h2 world</Heading>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
   );
