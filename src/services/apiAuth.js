039e6680435991252070b1c6c4f98be5c15a0b8c
ArtemLobastov <artemlobastov92@gmail.com>
2024-04-19 17:55:17 +0200

    Add user info update

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,23 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
+// NEW USER REGISTRATION
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: '',
+      },
+    },
+  });
+  if (error) {
+    throw new Error(error.message);
+  }
+  return data;
+}
+// USER LOGIN
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -10,7 +28,7 @@ export async function login({ email, password }) {
   }
   return data;
 }
-
+// USER'S DATA FETCH
 export async function getCurrentUser() {
   const { data: session } = await supabase.auth.getSession();
   if (!session.session) return null;
@@ -18,8 +36,34 @@ export async function getCurrentUser() {
   if (error) throw new Error(error.message);
   return data?.user;
 }
-
+// USER LOGOUT
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+// USER INFO UPDATE
+export async function updateUser({ password, fullName, avatar }) {
+  //password || fullName update
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+  // avatar upload in bucket
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from('avatars')
+    .upload(fileName, avatar);
+  if (storageError) throw new Error(storageError.message);
+
+  //UPDATE AVATAR IN USER INFO TO A NEW ONE AFTER SUCCESS UPLOAD
+  const { data: updatedUser, error: avatarUpdatingError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+  if (avatarUpdatingError) throw new Error(avatarUpdatingError.message);
+  return updatedUser;
 }
